411582589876304210b0837a9e2285f4e4369c32
Sarah Scott <s.j.scott84@gmail.com>
2017-06-15 11:32:39 -0700

    Add variables

--- chapter_1/SIMPLE.py
+++ chapter_1/SIMPLE.py
@@ -24,13 +24,33 @@ class Add:
     def reducible(self):
         return True
 
-    def reduce(self):
+    def reduce(self, environment):
         if self.left.reducible():
-            return Add(self.left.reduce(), self.right)
+            return Add(self.left.reduce(environment), self.right)
         elif self.right.reducible():
-            return Add(self.left, self.right.reduce())
+            return Add(self.left, self.right.reduce(environment))
         else:
             return Number(self.left.get() + self.right.get())
+
+
+class Subtract:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return '({} - {})'.format(self.left, self.right)
+
+    def reducible(self):
+        return True
+
+    def reduce(self, environment):
+        if self.left.reducible():
+            return Subtract(self.left.reduce(environment), self.right)
+        elif self.right.reducible():
+            return Subtract(self.left, self.right.reduce(environment))
+        else:
+            return Number(self.left.get() - self.right.get())
 
 
 class Multiply:
@@ -44,33 +64,79 @@ class Multiply:
     def reducible(self):
         return True
 
-    def reduce(self):
+    def reduce(self, environment):
         if self.left.reducible():
-            return Multiply(self.left.reduce(), self.right)
+            return Multiply(self.left.reduce(environment), self.right)
         elif self.right.reducible():
-            return Multiply(self.left, self.right.reduce())
+            return Multiply(self.left, self.right.reduce(environment))
         else:
             return Number(self.left.get() * self.right.get())
 
+
+class Divide:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return '({} / {})'.format(self.left, self.right)
+
+    def reducible(self):
+        return True
+
+    def reduce(self, environment):
+        if self.left.reducible():
+            return Divide(self.left.reduce(environment), self.right)
+        elif self.right.reducible():
+            return Divide(self.left, self.right.reduce(environment))
+        else:
+            return Number(self.left.get() / self.right.get())
+
+
+class Variable:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    def reducible(self):
+        return True
+
+    def reduce(self, environment):
+        return environment[self.name]
+
+
+
 class Machine:
-    def __init__(self, expression):
+    def __init__(self, expression, environment):
         self.expression = expression
+        self.environment = environment
         print self.expression
         self.run()
 
     def step(self):
-        self.expression = self.expression.reduce()
+        self.expression = self.expression.reduce(self.environment)
         print self.expression
 
     def run(self):
         while self.expression.reducible():
             self.step()
 
+#test = Machine(Variable('x'), {'x': Number(4)})
+#test = Machine(Add(Variable('x'), Variable('y')),
+#               {'x': Number(4), 'y': Number(10)})
 
+test2 = Machine(Add(Multiply(Variable('x'), Number(5)),
+                 Divide(Variable('y'), Number(2))),
+                 {'x': Number(5), 'y': Number(10)})
 
-calc = Machine(Add(Multiply(Number(1),Number(2)), 
-               Multiply(Number(3),Number(4))))
-number = Number(5)
+#calc = Machine(Subtract(Divide(Number(12),Number(2)), 
+#               Multiply(Number(3),Number(4))))
+#number = Number(5)
+
+#x = Variable('x')
+#print x
 
 #print calc
 #print calc.reducible()
